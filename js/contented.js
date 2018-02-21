f2245f7e64cc666b67e27e26e8352d704a5a8de4
Christian Murphy <cmurphy@unicon.net>
2018-02-21 14:59:51 -0700

    feat: make content slider responsive

--- js/contented.js
+++ js/contented.js
@@ -3,6 +3,29 @@ $(document).ready(function() {
     infinite: true,
     slidesToShow: 6,
     slidesToScroll: 1,
-    arrows: true
+    arrows: true,
+    responsive: [
+      {
+        breakpoint: 1000,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 4
+        }
+      },
+      {
+        breakpoint: 800,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      }
+    ]
   });
 });
